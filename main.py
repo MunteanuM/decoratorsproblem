73f554a0d74487fe113fffe852260c0a4a0f75fb
Mihai-Tudor MUNTEANU <mihamunteanu@pentalog.com>
2022-06-23 10:32:59 +0300

    new version

--- main.py
+++ main.py
@@ -5,27 +5,31 @@ def dice_calculator(func):
     '''decorator for setting the values of the dice'''
 
     def wrapper():
-        d1 = random.randint(1, 6)
-        d2 = random.randint(1, 6)
-        print('Computer: Dice #1: {} / Dice #2: {} / Sum: {}'.format(d1, d2, d1 + d2))
-
-        func()
-        return d1 + d2
+        result = func()
+        print('Dice stopped spinning')
+        return result
 
     return wrapper
 
+
 @dice_calculator
 def dice_throw():
-    print('Dice stopped spinning. Your turn')
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    print('Computer: Dice #1: {} / Dice #2: {} / Sum: {}'.format(d1, d2, d1 + d2))
+    return d1 + d2
 
 
 def player_dice(func):
     '''Decorator that receives parameters'''
 
     def wrapper(func_to_do, ref_value):
-        return func(func_to_do, ref_value)
+        result = func(func_to_do, ref_value)
+        print('Next Round:\n\n')
+        return result
 
     return wrapper
+
 
 @player_dice
 def dice_cheat(func_to_do, ref_value):
@@ -41,30 +45,46 @@ def dice_cheat(func_to_do, ref_value):
         return d1 + d2
 
 
+def repeat(answer):
+    '''decorator that recieves a parameter'''
+
+    def decorator(func):
+        def repeat_wrapper():
+            choice = answer
+            while choice == 'Y':
+                choice = func()
+            print('END GAME...')
+
+        return repeat_wrapper
+
+    return decorator
+
+
 def function_recall(func):
-    '''decorator that has the same name as function'''
+    '''decorator that has the same name'''
 
     def wrapper():
-        func()
+        answer = func(5)
+        return answer
 
     return wrapper
 
 
+@repeat('Y')
 @function_recall
-def function_recall():
-    replay = 'Y'
-    while replay == 'Y':
-        player_score = 0
-        computer_score = 0
-        for i in range(5):
-            computer_score = computer_score + dice_throw()
-            player_score = player_score + dice_cheat(input(), int(input()))
-        if player_score == computer_score:
-            print('You won!\nComputer_score: {} / Player_score: {}\nTo play again press Y else press N'.format(computer_score,player_score))
-        else:
-            print('You lost.\nComputer_score: {} / Player_score: {}\nTo play again press Y else press N'.format(computer_score,player_score))
-        replay = input()
-    print('GAME END...')
+def function_recall(n):
+    player_score = 0
+    computer_score = 0
+    for i in range(n):
+        computer_score = computer_score + dice_throw()
+        player_score = player_score + dice_cheat(input(), int(input()))
+    if player_score == computer_score:
+        print('You won!\nComputer_score: {} / Player_score: {}\nTo play again press Y else press N'.format(
+            computer_score, player_score))
+    else:
+        print('You lost.\nComputer_score: {} / Player_score: {}\nTo play again press Y else press N'.format(
+            computer_score, player_score))
+    return input()
 
 
 function_recall()
